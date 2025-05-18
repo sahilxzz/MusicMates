58942824c0aa8086a4dc72c8b95ac0cf367f9f18
Sahil <codekage9@gmail.com>
2025-05-18 18:24:07 +0530

    vite.config.js and main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-  <BrowserRouter basename="/MusicMates">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
   </ClerkProvider>
--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   base: '/MusicMates/',
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/music-mates/' : '/',
-    plugins: [react(),
-      tailwindcss(),
-    ],
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],      
 })
