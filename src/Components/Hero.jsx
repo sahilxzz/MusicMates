6fc4843ddbc2c08745f2876c7e8c2172a53ce700
Sahil <codekage9@gmail.com>
2025-05-18 22:38:01 +0530

    hero,jsx updates hero.css updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
     <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/hero-image.jpg")] bg-no-repeat bg-cover bg-center h-screen'>
         <div className='container'>
         <h1 className='heading'>Find people who listen to what you love!!!</h1>
-        <p className='para'>Signup.Connect to your spotify.Find your people</p>
+        <p className='para'>Signup. Connect to your spotify. Find your people</p>
 
         <div className='container2'>
         {user &&        
